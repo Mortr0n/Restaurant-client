50781d0452d96954c08939c9e684f030bbe18597
Mortron <cmsl4y3r@gmail.com>
2021-12-10 17:18:26 -0700

    finished create with validation

--- src/components/CreateRestaurant.js
+++ src/components/CreateRestaurant.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState} from 'react';
 import axios from 'axios';
-import { Link, Navigate } from '@reach/router';
+import { Link, navigate, Navigate } from '@reach/router';
+import '../App.css';
 
 const CreateRestaurant = (props) => {
     const [ name, setName ] = useState("");
     const [ address, setAddress ] = useState("");
+    const [ errors, setErrors ] = useState({}); 
     
-    
-
     const handleSubmit = (e) => {
         e.preventDefault();
+        // creating new object then using it on post
         const newRestaurant = {
             name,
             address
-        }
-        
-        axios.post(`http://localhost:8000/api/restaurant/`, newRestaurant)
+        };
+        // Could also build the object in the axios instead of calling the object
+        axios.post('http://localhost:8000/api/restaurant', newRestaurant)
             .then((res) => {
                 console.log(res.data);
+                navigate(`http://localhost:8000/api/restaurant/${res.data._id}`);
             })
             .catch((err) => {
                 console.log(err);
+                console.log(err.response);
+                // err.response is the body that you get in  postman
+                if(err.response.data.errors) {
+                    // save the errors in state so I can display them
+                    setErrors(err.response.data.errors);
+                }
             });
     }
-
-
 
     return(
         <div>
@@ -32,6 +38,11 @@ const CreateRestaurant = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Restaurant Name: </label>
+                    {/* display error messages on validation errors */}
+                    {
+                        errors.name &&
+                        <p className='errors'>{errors.name.message}</p>
+                    }
                     <input 
                         type="text" 
                         name="name"
@@ -41,6 +52,11 @@ const CreateRestaurant = (props) => {
                 </div>
                 <div>
                     <label>Address: </label>
+                    {/* display error messages on validation errors */}
+                    {
+                        errors.address &&
+                        <p className='errors'>{errors.address.message}</p>
+                    }
                     <input 
                         type="text" 
                         name="address"
